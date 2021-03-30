3ae3ec92620cb95e9e1746c93fb40c4f04805eae
marcazu <95marc@gmail.com>
2021-03-30 21:29:04 +0200

    PrimerCommit

--- src/main/java/com/init/productes/rest/ProductRest.java
+++ src/main/java/com/init/productes/rest/ProductRest.java
@@ -1,18 +1,33 @@
 package com.init.productes.rest;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.init.productes.entity.Product;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/products")
 public class ProductRest {
-	
+
+/*
 	//@GetMapping
 	@RequestMapping(value= "hello", method=RequestMethod.GET)
 	public String hello() {
 		return "Hello world";
+	}*/
+	@GetMapping
+	public ResponseEntity<List<Product>> getProduct(){
+		Product product = new Product();
+		product.setId(1l);
+		product.setName("producto1");
+		product.setDescripció("");
+		product.setTipus("");
+		return ResponseEntity.ok(product);
 	}
 
 }
