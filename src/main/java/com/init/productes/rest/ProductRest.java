15b67e10db2ceabf3922fcbe90fcfbf05d94be39
marcazu <95marc@gmail.com>
2021-03-31 17:51:33 +0200

    inicialitzant heroku

--- src/main/java/com/init/productes/rest/ProductRest.java
+++ src/main/java/com/init/productes/rest/ProductRest.java
@@ -1,33 +1,26 @@
 package com.init.productes.rest;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.init.productes.entity.Product;
-
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/")
 public class ProductRest {
 
-/*
+
 	//@GetMapping
 	@RequestMapping(value= "hello", method=RequestMethod.GET)
 	public String hello() {
 		return "Hello world";
-	}*/
+	}
+	
+	/*
 	@GetMapping
 	public ResponseEntity<List<Product>> getProduct(){
-		Product product = new Product();
-		product.setId(1l);
-		product.setName("producto1");
-		product.setDescripció("");
-		product.setTipus("");
+
 		return ResponseEntity.ok(product);
 	}
+	*/
 
 }
