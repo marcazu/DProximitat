3ec73a9e1532e1fdeba32c111c08b7e4dc911af5
marcazu <95marc@gmail.com>
2021-05-24 00:10:55 +0200

    arreglatn cosetes

--- src/main/java/com/init/productes/Dto/ProducteIdQuantitatDto.java
+++ src/main/java/com/init/productes/Dto/ProducteIdQuantitatDto.java
@@ -4,6 +4,14 @@ public class ProducteIdQuantitatDto {
 	private String producteId;
 	private String Quantitat;
 	
+	public ProducteIdQuantitatDto() {
+		super();
+	}
+	public ProducteIdQuantitatDto(String producteId, String quantitat) {
+		super();
+		this.producteId = producteId;
+		Quantitat = quantitat;
+	}
 	public String getProducteId() {
 		return producteId;
 	}
