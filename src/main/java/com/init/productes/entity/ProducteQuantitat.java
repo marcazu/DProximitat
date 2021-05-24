df8ec2c2459d0273b1ff8bbdd82a250e863312d9
marcazu <95marc@gmail.com>
2021-05-24 02:16:02 +0200

    arreglant + costes

--- src/main/java/com/init/productes/entity/ProducteQuantitat.java
+++ src/main/java/com/init/productes/entity/ProducteQuantitat.java
@@ -3,6 +3,8 @@ package com.init.productes.entity;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -16,6 +18,7 @@ public class ProducteQuantitat {
 
 	@Id
 	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	@Column(name="quantitat")
 	private int quantitat;
