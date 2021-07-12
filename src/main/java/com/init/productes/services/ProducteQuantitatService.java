8dde79d970d56e0589e0ead16778be7847305aa2
marcazu <95marc@gmail.com>
2021-07-12 18:48:18 +0200

    arreglant errors commit 102

--- src/main/java/com/init/productes/services/ProducteQuantitatService.java
+++ src/main/java/com/init/productes/services/ProducteQuantitatService.java
@@ -43,7 +43,7 @@ public class ProducteQuantitatService {
 		
 	}
 	public List<ProducteQuantitat> getByComandaId(Long comandaId) {
-		List<ProducteQuantitat> pq = pqRepository.findByproducteQuantitatIDcomandaId(comandaId);
+		List<ProducteQuantitat> pq = pqRepository.findByproducteQuantitatID_comandaId(comandaId);
 		if(pq.isEmpty()) throw new ApiRequestException("No hi ha cap producte quantitat a la BD");
 		return pq;
 	}
